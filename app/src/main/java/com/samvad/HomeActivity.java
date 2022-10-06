35360ee844445d79793088af225dcc5574adab30
BrijeshJagad <jagadbrijesh1501@gmail.com>
2022-10-06 21:58:10 +0530

    added logout, conformation screen on logou.

--- app/src/main/java/com/samvad/HomeActivity.java
+++ app/src/main/java/com/samvad/HomeActivity.java
@@ -1,32 +1,52 @@
 package com.samvad;
 
 import android.Manifest;
+import android.app.Dialog;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Collections;
 
 public class HomeActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     public static final String TAG = "HomeActivity";
     private static final int CAMERA_PERMISSION = 104;
     SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
     private CameraSource cameraSource;
+    private UIUpdater uiUpdater;
 
+    LinearLayout speakBtn, speakBgBtn;
+    TextView translatedText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,22 +54,32 @@ public class HomeActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         setContentView(R.layout.activity_home);
 
         surfaceView = findViewById(R.id.surface_view);
-        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(HomeActivity.this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION);
-        }else{
-            startCameraSource();
-        }
+        speakBtn = findViewById(R.id.speak_btn);
+        speakBgBtn = findViewById(R.id.speak_bg_btn);
+        translatedText = findViewById(R.id.translated_text);
+        speakBtn.setOnClickListener((v)->{
+
+        });
+        startCameraSource();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        startCameraSource();
-        if(grantResults.length!=0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
-            startCameraSource();
-        }else{
-            Log.d(TAG, "onRequestPermissionsResult: ");
+    /**
+     * Take a screenshot of the view
+     *
+     * @param view the view to capture
+     * @return the bitmap representing the pixels of the given view
+     */
+    @Nullable
+    public static Bitmap loadBitmapFromView(View view) {
+        try {
+            view.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+            view.setDrawingCacheEnabled(false);
+            return bitmap;
+        } catch (OutOfMemoryError error) {
+            Log.d(TAG, "Out of Memory while loadBitmapFromView");
+            return null;
         }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
@@ -57,9 +87,25 @@ public class HomeActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         switch (item.getItemId()) {
             case R.id.about:
                 Toast.makeText(this, "About Clicked!!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, AboutActivity.class));
                 return true;
             case R.id.logout:
-                Toast.makeText(this, "Logout Clicked!!", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(this, "Logout Clicked!!", Toast.LENGTH_SHORT).show();
+                Dialog customDialog = new Dialog(this);
+                customDialog.getWindow().setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT);
+                customDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                customDialog.getWindow().getAttributes().windowAnimations = android.R.style.Animation_Dialog;
+                customDialog.setContentView(R.layout.push_dialog);
+                customDialog.show();
+                LinearLayout btn_yes = customDialog.findViewById(R.id.btn_yes);
+                btn_yes.setOnClickListener(v -> {
+                    logout();
+                    customDialog.cancel();
+                });
+                LinearLayout btn_no = customDialog.findViewById(R.id.btn_no);
+                btn_no.setOnClickListener((v) -> {
+                    customDialog.cancel();
+                });
                 return true;
             default:
                 return false;
@@ -72,6 +118,7 @@ public class HomeActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         popup.inflate(R.menu.home_menu);
         popup.show();
     }
+
     private void startCameraSource() {
         if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(HomeActivity.this, "Cannot start camera", Toast.LENGTH_SHORT).show();
@@ -82,14 +129,11 @@ public class HomeActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int width = displayMetrics.widthPixels;
-        cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
-                .setFacing(CameraSource.CAMERA_FACING_BACK)
-                .setRequestedPreviewSize((width/2)*4/3, width/2)
-                .setAutoFocusEnabled(true)
-                .setRequestedFps(2.0f)
-                .build();
-        surfaceView.getHolder().setFixedSize(width/2, (width/2)*4/3);
-        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+        cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer).setFacing(CameraSource.CAMERA_FACING_BACK).setRequestedPreviewSize(width * 4 / 3, width).setAutoFocusEnabled(true).setRequestedFps(2.0f).build();
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.setFixedSize(width, width * 4 / 3);
+
+        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(@NonNull SurfaceHolder holder) {
                 try {
@@ -98,13 +142,14 @@ public class HomeActivity extends AppCompatActivity implements PopupMenu.OnMenuI
                         return;
                     }
                     cameraSource.start(surfaceView.getHolder());
+                    startProcessing();
                 } catch (Exception e) {
                     Log.d(TAG, "surfaceCreated: " + e.getMessage());
                 }
             }
 
             @Override
-            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {//Release source for cameraSource
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {//Release source for cameraSourc
 
             }
 
@@ -112,16 +157,133 @@ public class HomeActivity extends AppCompatActivity implements PopupMenu.OnMenuI
             public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
                 cameraSource.stop();
             }
+
+            //            Log.d(TAG, "surfaceChanged: "+bitmap);
         });
     }
 
+    private void startProcessing() {
+
+        uiUpdater = new UIUpdater(() -> {
+            Bitmap bitmap = loadBitmapFromView(surfaceView);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
+            byte[] byteArray = stream.toByteArray();
+            StringBuffer sb = new StringBuffer();
+            Log.d(TAG, "startProcessing: " + byteArray.length);
+//            for (byte b:byteArray) {
+//                sb.append(b);
+//            }
+            Log.d(TAG, "" + Collections.singletonList(byteArray));
+//            Log.d(TAG, ""+sb.toString());
+
+//            Bitmap compressedBitmap = BitmapFactory.decodeByteArray(byteArray,0,byteArray.length);
+            Bitmap scaled = Bitmap.createScaledBitmap(bitmap, bitmap.getWidth() / 2, bitmap.getHeight() / 2, true);
+//            Log.d(TAG, "startProcessing: " + scaled.getHeight());
+//            Log.d(TAG, "startProcessing: " + scaled.getWidth());
+//            Log.d(TAG, "startProcessing: " + );
+//            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//            scaled.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+//            byte[] byteArray = stream.toByteArray();
+//            Log.d(TAG, "startProcessing: "+byteArray.length);
+//            scaled.recycle();
+            scaled.getAllocationByteCount();
+
+            Log.d(TAG, "startProcessing: " + scaled.getAllocationByteCount());
+        }, 100);
+        uiUpdater.startUpdates();
+    }
 
     private void logout() {
 
+        GoogleSignInOptions gso;
+        GoogleSignInClient gsc;
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+        gsc.signOut().addOnCompleteListener(task -> {
+            startActivity(new Intent(this, LoginActivity.class));
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        });
     }
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
         super.onPointerCaptureChanged(hasCapture);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        uiUpdater.startUpdates();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        uiUpdater.stopUpdates();
+    }
+
+    /**
+     * A class used to perform periodical updates,
+     * specified inside a runnable object. An update interval
+     * may be specified (otherwise, the class will perform the
+     * update every 2 seconds).
+     *
+     * @author Carlos Simões
+     */
+    public class UIUpdater {
+        // Create a Handler that uses the Main Looper to run in
+        private final Handler mHandler = new Handler(Looper.getMainLooper());
+
+        private final Runnable mStatusChecker;
+        private int UPDATE_INTERVAL = 1000;
+
+        /**
+         * Creates an UIUpdater object, that can be used to
+         * perform UIUpdates on a specified time interval.
+         *
+         * @param uiUpdater A runnable containing the update routine.
+         */
+        public UIUpdater(final Runnable uiUpdater) {
+            mStatusChecker = new Runnable() {
+                @Override
+                public void run() {
+                    // Run the passed runnable
+                    uiUpdater.run();
+                    // Re-run it after the update interval
+                    mHandler.postDelayed(this, UPDATE_INTERVAL);
+                }
+            };
+        }
+
+        /**
+         * The same as the default constructor, but specifying the
+         * intended update interval.
+         *
+         * @param uiUpdater A runnable containing the update routine.
+         * @param interval  The interval over which the routine
+         *                  should run (milliseconds).
+         */
+        public UIUpdater(Runnable uiUpdater, int interval) {
+            this(uiUpdater);
+            UPDATE_INTERVAL = interval;
+        }
+
+        /**
+         * Starts the periodical update routine (mStatusChecker
+         * adds the callback to the handler).
+         */
+        public synchronized void startUpdates() {
+            mStatusChecker.run();
+        }
+
+        /**
+         * Stops the periodical update routine from running,
+         * by removing the callback.
+         */
+        public synchronized void stopUpdates() {
+            mHandler.removeCallbacks(mStatusChecker);
+        }
     }
 }
--- app/src/main/java/com/samvad/LoginActivity.java
+++ app/src/main/java/com/samvad/LoginActivity.java
@@ -1,14 +1,18 @@
 package com.samvad;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-import com.google.android.gms.auth.api.identity.BeginSignInRequest;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -18,59 +22,83 @@ import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public static final int GOOGLE_SIGN_IN_CODE = 100;
+    private static final String TAG = "LoginActivity";
     LinearLayout btnGoogle;
     GoogleSignInOptions gso;
     GoogleSignInClient gsc;
-    public static final int GOOGLE_SIGN_IN_CODE = 100;
+    private static final int CAMERA_PERMISSION = 104;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        if(checkForSignIn()){
-            navigateToHomeActivity();
+        if (checkForSignIn()) {
+            if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(LoginActivity.this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION);
+            } else {
+                navigateToHomeActivity();
+            }
         }
         btnGoogle = findViewById(R.id.btn_google);
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
-        gsc = GoogleSignIn.getClient(this,gso);
+        gsc = GoogleSignIn.getClient(this, gso);
 
         btnGoogle.setOnClickListener(v -> signIn());
     }
 
     private boolean checkForSignIn() {
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        return account!=null;
+        return account != null;
     }
 
     private void signIn() {
         Intent intent = gsc.getSignInIntent();
-        startActivityForResult(intent,GOOGLE_SIGN_IN_CODE);
+        startActivityForResult(intent, GOOGLE_SIGN_IN_CODE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == GOOGLE_SIGN_IN_CODE){
+        if (requestCode == GOOGLE_SIGN_IN_CODE) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 task.getResult(ApiException.class);
 
                 GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-                Toast.makeText(this,"hello "+account.getDisplayName(), Toast.LENGTH_SHORT).show();
-                navigateToHomeActivity();
+                Toast.makeText(this, "hello " + account.getDisplayName(), Toast.LENGTH_SHORT).show();
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(LoginActivity.this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION);
+                } else {
+                    navigateToHomeActivity();
+                }
             } catch (ApiException e) {
                 e.printStackTrace();
-                Toast.makeText(this,"Something went Wrong.",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Something went Wrong.", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            navigateToHomeActivity();
+        } else {
+            Log.d(TAG, "onRequestPermissionsResult: ");
+            ActivityCompat.requestPermissions(LoginActivity.this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION);
+//            navigateToHomeActivity();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
     private void navigateToHomeActivity() {
-        startActivity(new Intent(this,HomeActivity.class));
+        startActivity(new Intent(this, HomeActivity.class));
+
         finish();
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
